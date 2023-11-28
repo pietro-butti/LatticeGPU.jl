d58f236d8c085abb962827c9dc739c91830f69d4
Fernando P. Panadero <fernando.p@csic.es>
2023-11-28 15:30:00 +0100

    Dirac documentation

--- docs/make.jl
+++ docs/make.jl
@@ -10,5 +10,6 @@ makedocs(sitename="LatticeGPU", modules=[LatticeGPU], doctest=true,
              "Space-time" => "space.md",
              "Groups and algebras" => "groups.md",
              "Fields" => "fields.md"
+             "Dirac" => "dirac.md"
              ], 
          repo = "https://igit.ific.uv.es/alramos/latticegpu.jl")
